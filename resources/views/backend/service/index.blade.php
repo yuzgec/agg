73c310c73c65a941046f179982a113309d1a8df3
yuzgec <olcayy@gmail.com>
2024-01-10 18:53:13 +0300

    Fix

--- resources/views/backend/service/index.blade.php
+++ resources/views/backend/service/index.blade.php
@@ -1,5 +1,5 @@
 @extends('backend.layout.app')
-@section('title', 'Hizmet Listele')
+@section('title', {{ request('name') }}.' Listele')
 @section('content')
     <div class="col-12 col-md-8">
         <div class="card">
@@ -66,7 +66,7 @@
                                         Eylemler
                                     </button>
                                     <div class="dropdown-menu dropdown-menu-end">
-                                        <a class="dropdown-item justify-content-between" href="{{ route('service.edit', $item->id) }}" title="Düzenle">
+                                        <a class="dropdown-item justify-content-between" href="{{ route('service.edit', [$item->id, 'category' => request('category'), 'name' => request('name')]) }}" title="Düzenle">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 15l8.385 -8.415a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3z" /><path d="M16 5l3 3" /><path d="M9 7.07a7.002 7.002 0 0 0 1 13.93a7.002 7.002 0 0 0 6.929 -5.999" /></svg>
                                             Düzenle
                                         </a>
